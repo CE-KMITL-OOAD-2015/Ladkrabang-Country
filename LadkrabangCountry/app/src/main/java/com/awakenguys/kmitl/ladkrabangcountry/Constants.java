73c158894214b21592d93d82d4f012f47ca0bc96
Sirapat <kingkab1317@hotmail.com>
2015-09-11 01:35:28 +0700

    Initialize (import from king)

---
+++ LadkrabangCountry/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Constants.java
@@ -0,0 +1,11 @@
+package com.awakenguys.kmitl.ladkrabangcountry;
+
+import android.provider.BaseColumns;
+
+public interface Constants extends BaseColumns {
+    String TABLE_NAME = "buildings";
+    String TITLE = "title";
+    String FACULTY = "faculty";
+    String LAT = "lat";
+    String LNG = "lng";
+}
