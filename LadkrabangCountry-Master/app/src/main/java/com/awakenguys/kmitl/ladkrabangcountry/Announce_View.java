67ee6519864d17a0b48e8219f6a744d4c7d2f2a3
oilsrp23346 <oilsrp23346@gmail.com>
2015-11-17 19:35:59 +0700

    AddlistView
    not complete

--- LadkrabangCountry-Master/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Announce_View.java
+++ LadkrabangCountry-Master/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Announce_View.java
@@ -9,24 +9,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Announce_View extends AppCompatActivity {
-
+    ListView listView;
+    String[] values;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announce__view);
-/*        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        listView = (ListView)findViewById(R.id.myListView);
+        values = new String[] {
+                "Android","iphone","Waaa","sssss","ssssss","aaaaaa"
+        };
 
-            }
-
-        });*/
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
