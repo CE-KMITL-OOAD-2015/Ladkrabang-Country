9a8a9a8446c94f375fa5b77f645d2e52c35c1ab6
oilsrp23346 <oilsrp23346@gmail.com>
2015-11-17 23:39:59 +0700

    Vbug
    edit

--- LadkrabangCountry-Master/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Review_View.java
+++ LadkrabangCountry-Master/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Review_View.java
@@ -13,7 +13,7 @@ public class Review_View extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review_View);
+        setContentView(R.layout.activity_review_view);
     }
 
 
