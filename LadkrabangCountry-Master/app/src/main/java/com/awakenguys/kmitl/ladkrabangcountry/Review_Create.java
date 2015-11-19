1aae1ed101805535c41d242e03b2fb881f41d828
Xynchronize <x_zeed_kz@hotmail.com>
2015-11-19 23:02:09 +0700

    failed

--- LadkrabangCountry-Master/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Review_Create.java
+++ LadkrabangCountry-Master/app/src/main/java/com/awakenguys/kmitl/ladkrabangcountry/Review_Create.java
@@ -13,6 +13,7 @@ import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,16 +23,31 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.awakenguys.kmitl.ladkrabangcountry.model.Review;
 import com.awakenguys.kmitl.ladkrabangcountry.model.User;
 
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
 import java.io.BufferedReader;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Review_Create extends AppCompatActivity {
@@ -43,6 +59,8 @@ public class Review_Create extends AppCompatActivity {
     String filePath = "";
     String topic;
     String content;
+    String encodedImg = null;
+    RestTemplate rest = new RestTemplate();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,26 +123,79 @@ public class Review_Create extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
     private void post(){
         //magePost();
+
+
+
+
+
+
         EditText txtDescription = (EditText) findViewById(R.id.edit_text);
         EditText txtDescription2 = (EditText) findViewById(R.id.edit_text_2);
-        String topic = txtDescription.getText().toString();
-        String content = txtDescription2.getText().toString();
-        HTTPRequest rq = new HTTPRequest();
+        topic = txtDescription.getText().toString();
+        content = txtDescription2.getText().toString();
+
+        new PostTask().execute();
+
+
+    ;
+        /*HTTPRequest rq = new HTTPRequest();
         try {
-            rq.execute("http://203.151.92.199:8888/addreview?topic=" + topic + "&content=" +
-                    content + "&authorId=" + Profile.getUser().getId());
+            if (encodedImg == null) {
+                rq.execute("http://203.151.92.199:8888/addreview?topic=" + topic + "&content=" +
+                        content + "&authorId=" + Profile.getUser().getId());
+            }
+            else {
+                rq.execute("http://203.151.92.199:8888/addreview?topic=" + topic + "&content=" +
+                        content + "&authorId=" + Profile.getUser().getId()+"&img="+ encodedImg);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
 
         Toast.makeText(Review_Create.this, "Success!", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, Review_View.class));
 
     }
 
+    private class PostTask extends AsyncTask<Void,Void,String>{
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+                map.add("topic", topic);
+                map.add("content", content);
+                map.add("authorId", Profile.getUser().getId());
+                map.add("img", encodedImg);
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+                HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+                HttpMessageConverter formHttpMessageConverter = new FormHttpMessageConverter();
+                HttpMessageConverter stringHttpMessageConverternew = new StringHttpMessageConverter();
+                List<HttpMessageConverter<?>> converters = new ArrayList<>();
+                converters.add(formHttpMessageConverter);
+                converters.add(stringHttpMessageConverternew);
+                rest.setMessageConverters(converters);
+
+                String result = rest.postForObject("http://203.151.91.199:8888/addreviewwithimg", map, String.class);
+                return result;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(Review_Create.this, s, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -147,85 +218,6 @@ public class Review_Create extends AppCompatActivity {
 
     }
 
-    /*private String imagePost(String filePath) {
-        FTPClient ftpClient = new FTPClient();
-        FileInputStream fis = null;
-        boolean result;
-        String ftpServerAddress = "localhost";
-        String userName = "root";
-        String password = "kingkikkok";
-
-        if (bitmap != null) {
-            ftpClient = new FTPClient();
-            try {
-                ftpClient.connect("203.151.92.199");
-                result = ftpClient.login(userName, password);
-                if (result == true) {
-                    System.out.println("Logged in Successfully !");
-                } else {
-                    System.out.println("Login Fail!");
-                    return null;
-                }
-                ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-                ftpClient.changeWorkingDirectory("/images");
-                File file = new File(filePath);
-                String fileName = file.getName();
-                fis = new FileInputStream(file);
-                result = ftpClient.storeFile(fileName, fis);
-                if (result == true) {
-                    System.out.println("File is uploaded successfully");
-                    filePath = file.getName();
-                } else {
-                    System.out.println("File uploading failed");
-                }
-                ftpClient.logout();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    ftpClient.disconnect();
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-            }
-
-
-            /*URL url;
-            HttpURLConnection connection = null;
-            try {
-                //Create connection
-                url = new URL("http://203.151.92.199:8888/uploadimage");
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setReadTimeout(10000);
-                connection.setConnectTimeout(15000);
-                connection.setDoOutput(true);
-                connection.setDoOutput(true);
-                connection.setRequestProperty("Content-Type", "image/jpeg");
-                connection.setRequestMethod("POST");
-
-
-                OutputStream outputStream = connection.getOutputStream();
-
-
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-
-                outputStream.close();
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
-                while((inputLine = in.readLine())!= null){
-                    response.append(inputLine);
-                }
-                in.close();
-                connection.disconnect();
-                return response.toString();
-            } catch (Exception e) {
-                e.printStackTrace();
-
-            }
-        }
-        return null;
-    }*/
 
     public void decodeFile(String filePath) {
 
@@ -235,7 +227,7 @@ public class Review_Create extends AppCompatActivity {
         BitmapFactory.decodeFile(filePath, o);
 
         // The new size we want to scale to
-        final int REQUIRED_SIZE = 400;
+        final int REQUIRED_SIZE = 300;
 
         // Find the correct scale value. It should be the power of 2.
         int width_tmp = o.outWidth, height_tmp = o.outHeight;
@@ -250,10 +242,40 @@ public class Review_Create extends AppCompatActivity {
 
         // Decode with inSampleSize
         BitmapFactory.Options o2 = new BitmapFactory.Options();
-        o2.inSampleSize = scale;
+
         bitmap = BitmapFactory.decodeFile(filePath, o2);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 0, baos);
+        byte[] b = baos.toByteArray();
         imageButton.setImageBitmap(bitmap);
         imageButton.setBackgroundResource(0);
+        encodedImg = Base64.encodeToString(b, Base64.URL_SAFE);
+                /*replace("%", "%25").
+                replace(" ", "%20").
+                replace("$", "%24").
+                replace("&", "%60").
+                replace("`", "%60").
+                replace(":", "%3A").
+                replace("<", "%3C").
+                replace(">", "%3E").
+                replace("[", "%5B").
+                replace("]", "%5D").
+                replace("{", "%7B").
+                replace("}", "%7D").
+                replace("“", "%22").
+                replace("+", "%2B").
+                replace("#", "%23").
+                replace("@", "%40").
+                replace("/", "%2F").
+                replace(";", "%3B").
+                replace("=", "%3D").
+                replace("?", "%23").
+                replace("\\", "%5C").
+                replace("^", "%5E").
+                replace("|", "%7C").
+                replace("~", "%7E").
+                replace("‘", "%27").
+                replace(",", "%2C");*/
 
     }
 }
